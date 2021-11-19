529fa53211c5e1aa50a62bc2173bb2a7c876a15c
Jubair Istiaq Khan <superanonto001@gmail.com>
2021-11-19 23:56:19 +0600

    kosom last commit

--- src/Pages/Dashboard/ManageAllOrder/ManageAllOrder.js
+++ src/Pages/Dashboard/ManageAllOrder/ManageAllOrder.js
@@ -1,8 +1,4 @@
 import React from 'react';
-import Grid from '@mui/material/Grid';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
 import "./ManageAllOrder.css"
 
 const ManageAllOrder = props => {
@@ -42,94 +38,6 @@ const ManageAllOrder = props => {
                             disabled
                             defaultValue={number} />
                     </div>
-
-                    {/* <table class="table form-style">
-                    <thead>
-                        <tr>
-                            <th scope="col">Product Name</th>
-                            <th scope="col">Customer</th>
-                            <th scope="col">Email</th>
-                            <th scope="col">Address</th>
-                            <th scope="col">Phone Number</th>
-                        </tr>
-                    </thead>
-                    <div> */}
-                    {/* <tbody>
-                            <tr> */}
-                    {/* <th scope="row">1</th> */}
-                    {/* <td>{productName}</td>
-                                <td>{name}</td>
-                                <td>{email}</td>
-                                <td>{address}</td>
-                                <td>{number}</td>
-                            </tr> */}
-                    {/* <tr>
-                            <th scope="row">2</th>
-                            <td>Jacob</td>
-                            <td>Thornton</td>
-                            <td>@fat</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">3</th>
-                            <td colspan="2">Larry the Bird</td>
-                            <td>@twitter</td>
-                        </tr> */}
-                    {/* </tbody>
-                    </div>
-
-                </table> */}
-
-                    {/* <div className="form-style">
-                    <table>
-                        <tr>
-                            <th>Product</th>
-                            <th>Username</th>
-                            <th>email</th>
-                            <th>address</th>
-                            <th>phone number</th>
-                        </tr>
-                        <tr>
-                            <td>
-                                <input type="email"
-                                    class="form-control"
-                                    id="floatingInputGrid"
-                                    disabled
-                                    defaultValue={productName} />
-                            </td>
-                            <td>
-                                <input type="email"
-                                    class="form-control"
-                                    id="floatingInputGrid"
-                                    disabled
-                                    defaultValue={name} />
-                            </td>
-                            <td>
-                                <input type="email"
-                                    class="form-control"
-                                    id="floatingInputGrid"
-                                    disabled
-                                    defaultValue={email}
-                                />
-                            </td>
-                            <td>
-                                <input type="email"
-                                    class="form-control"
-                                    id="floatingInputGrid"
-                                    disabled
-                                    defaultValue={address} />
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <input type="email"
-                                    class="form-control"
-                                    id="floatingInputGrid"
-                                    disabled
-                                    defaultValue={number} />
-                            </td>
-                        </tr>
-                    </table>
-                </div> */}
                 </div>
             </div>
         </div>
