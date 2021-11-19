bf9188f713f55fdee92ded4bdeb9c86ec32f63c9
Jubair Istiaq Khan <superanonto001@gmail.com>
2021-11-19 23:47:44 +0600

    last commit

--- src/Pages/Dashboard/ManageAllOrder/ManageAllOrder.js
+++ src/Pages/Dashboard/ManageAllOrder/ManageAllOrder.js
@@ -9,35 +9,127 @@ const ManageAllOrder = props => {
     const { name, email, address, number, productName } = props.order;
     return (
         <div>
-            <div class="row g-2 ">
-                <div class="col-md d-flex">
+
+            <div className="row g-2">
+                <div className="col-md form-style d-flex">
                     <div></div>
-                    <input type="email"
-                        class="form-control"
-                        id="floatingInputGrid"
-                        disabled
-                        defaultValue={productName} />
-                    <input type="email"
-                        class="form-control"
-                        id="floatingInputGrid"
-                        disabled
-                        defaultValue={name} />
-                    <input type="email"
-                        class="form-control"
-                        id="floatingInputGrid"
-                        disabled
-                        defaultValue={email}
-                    />
-                    <input type="email"
-                        class="form-control"
-                        id="floatingInputGrid"
-                        disabled
-                        defaultValue={address} />
-                    <input type="email"
-                        class="form-control"
-                        id="floatingInputGrid"
-                        disabled
-                        defaultValue={number} />
+                    <div class="col-md d-flex">
+                        <div></div>
+                        <input type="email"
+                            class="form-control"
+                            id="floatingInputGrid"
+                            disabled
+                            defaultValue={productName} />
+                        <input type="email"
+                            class="form-control"
+                            id="floatingInputGrid"
+                            disabled
+                            defaultValue={name} />
+                        <input type="email"
+                            class="form-control"
+                            id="floatingInputGrid"
+                            disabled
+                            defaultValue={email}
+                        />
+                        <input type="email"
+                            class="form-control"
+                            id="floatingInputGrid"
+                            disabled
+                            defaultValue={address} />
+                        <input type="email"
+                            class="form-control"
+                            id="floatingInputGrid"
+                            disabled
+                            defaultValue={number} />
+                    </div>
+
+                    {/* <table class="table form-style">
+                    <thead>
+                        <tr>
+                            <th scope="col">Product Name</th>
+                            <th scope="col">Customer</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Address</th>
+                            <th scope="col">Phone Number</th>
+                        </tr>
+                    </thead>
+                    <div> */}
+                    {/* <tbody>
+                            <tr> */}
+                    {/* <th scope="row">1</th> */}
+                    {/* <td>{productName}</td>
+                                <td>{name}</td>
+                                <td>{email}</td>
+                                <td>{address}</td>
+                                <td>{number}</td>
+                            </tr> */}
+                    {/* <tr>
+                            <th scope="row">2</th>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>@fat</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td colspan="2">Larry the Bird</td>
+                            <td>@twitter</td>
+                        </tr> */}
+                    {/* </tbody>
+                    </div>
+
+                </table> */}
+
+                    {/* <div className="form-style">
+                    <table>
+                        <tr>
+                            <th>Product</th>
+                            <th>Username</th>
+                            <th>email</th>
+                            <th>address</th>
+                            <th>phone number</th>
+                        </tr>
+                        <tr>
+                            <td>
+                                <input type="email"
+                                    class="form-control"
+                                    id="floatingInputGrid"
+                                    disabled
+                                    defaultValue={productName} />
+                            </td>
+                            <td>
+                                <input type="email"
+                                    class="form-control"
+                                    id="floatingInputGrid"
+                                    disabled
+                                    defaultValue={name} />
+                            </td>
+                            <td>
+                                <input type="email"
+                                    class="form-control"
+                                    id="floatingInputGrid"
+                                    disabled
+                                    defaultValue={email}
+                                />
+                            </td>
+                            <td>
+                                <input type="email"
+                                    class="form-control"
+                                    id="floatingInputGrid"
+                                    disabled
+                                    defaultValue={address} />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <input type="email"
+                                    class="form-control"
+                                    id="floatingInputGrid"
+                                    disabled
+                                    defaultValue={number} />
+                            </td>
+                        </tr>
+                    </table>
+                </div> */}
                 </div>
             </div>
         </div>
--- src/Pages/Dashboard/ManageAllOrders/ManageAllOrders.js
+++ src/Pages/Dashboard/ManageAllOrders/ManageAllOrders.js
@@ -23,6 +23,13 @@ const ManageAllOrders = () => {
                     <Typography sx={{ fontWeight: 600, m: 5, margin: '70px 0px', color: '#0F8C9C' }} variant="h4" component="div">
                         All Orders
                     </Typography>
+                    <div className="d-flex my-5">
+                        <input type="text" disabled defaultValue="product name" />
+                        <input type="text" disabled defaultValue="customer name" />
+                        <input type="text" disabled defaultValue="email" />
+                        <input type="text" disabled defaultValue="address" />
+                        <input type="text" disabled defaultValue="phone number" />
+                    </div>
                     <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
                         {
                             orders.map(order => <ManageAllOrder
